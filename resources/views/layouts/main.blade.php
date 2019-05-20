ff474b55173998f2f46ecc25161556d3e220e3fa
Antoine <antoineperrenoud02@gmail.com>
2019-05-20 22:52:57 +0200

    Create a header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,6 +39,56 @@
 		<div class="container-fluid col-md-10 offset-md-1">
 			@yield('content')
 		</div>
+		@section('footer')
+		<footer>
+			<div class="container-fluid text-center">
+				<div class="row">
+					<div class="container-fluid text-md-left">
+						<div class="row">
+							<div class="col-md-5 offset-md-2">
+							<h5 class="text-uppercase font-weight-bold">atnprd.fr</h5>
+							<p>
+								Ce site est une présentation de mon travail et de mon CV, mais c'est aussi un site d'expérimentation. Si vous rencontrez d'éventuels problèmes ou bugs, n'hésitez pas à me les faire remarquer !
+							</p>
+						</div>
+						<div class="col-md-3 offset-md-2">
+							<h5 class="font-weight-bold text-uppercase">Catégories</h5>
+							<ul class="list-unstyled">
+								<li>
+									<a href="#!">Présentation</a>
+								</li>
+								<li>
+									<a href="#!">Parcours</a>
+								</li>
+								<li>
+									<a href="#!">Compétences</a>
+								</li>
+								<li>
+									<a href="#!">Réalisations</a>
+								</li>
+							</ul>
+						</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="d-flex justify-content-center flex-center">
+							<a>
+								<i class="fab fa-facebook-f fa-2x"> </i>
+							</a>
+							<a>
+								<i class="far fa-envelope fa-2x"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="text-center">
+				<a href="#">atnprd.fr </a> © 2019
+			</div>
+		</footer>
+		@show
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
